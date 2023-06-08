92e93c7092048e8f5e1aad378a637fd2685a42e1
Retsi <Branrcak@outlook.com>
2023-06-08 17:36:01 +0700

    Make a Route and Link to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="post">My First Post</a></h1>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed nisi delectus sapiente ea magni veniam voluptate praesentium. In, temporibus. Illo temporibus neque aut necessitatibus aliquid quos officiis optio non sit hic aperiam debitis nulla vero, ipsam voluptatibus nam molestiae, esse qui sunt blanditiis tempore. Repellat dolores aperiam sed, aspernatur consectetur corporis ipsa explicabo doloremque commodi veniam? Facere quae quam maxime quasi est amet adipisci eius qui quidem repellendus officiis, sunt exercitationem, dolorem ducimus suscipit dolor possimus perferendis a minima repellat accusamus laudantium voluptate nam itaque. Debitis omnis et sint nam assumenda esse accusantium sequi, quis porro officia unde cupiditate suscipit?</p>
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
